0d513da893055114fbe865a12aaf6a8286140bf4
Hugo Fernandes <huguinho.p.fernandes@gmail.com>
2021-08-14 19:16:31 -0300

    11th class

---
+++ src/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 23
+
+console.log(`A sua velocidade é de ${vel} Km/h.`)
+
+if (vel > 60) {
+    console.log('A sua velocidade é maior do que o limite permitido!')
+    console.log('Multado!')
+}
+
+console.log('Dirija sempre com o cinto de segurança!')
