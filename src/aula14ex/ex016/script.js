45991c20e320eb480c8b1d0e8b928a54f2eebbce
Hugo Fernandes <huguinho.p.fernandes@gmail.com>
2021-09-02 20:04:04 -0300

    Ex016 finished

--- src/aula14ex/ex016/script.js
+++ src/aula14ex/ex016/script.js
@@ -1,10 +1,23 @@
 function count() {
-    var start = Number(document.getElementById('inputStart').value)
-    var end = Number(document.getElementById('inputEnd').value)
-    var step = Number(document.getElementById('inputStep').value)
-
-    var divRes = document.getElementById('res')
-    var outputPar = document.createElement('p')
+    let start = Number(document.getElementById('inputStart').value)
+    let end = Number(document.getElementById('inputEnd').value)
+    let step = Number(document.getElementById('inputStep').value)
+    
+    if (start === 0) {
+        document.getElementById('inputStart').value = '0'
+    }
+    
+    if (end === 0) {
+        document.getElementById('inputEnd').value = '0'
+    }
+    
+    if (step < 1) {
+        alert('O passo deve ser 1 ou mais!')
+        return
+    }
+    
+    let divRes = document.getElementById('res')
+    let outputPar = document.createElement('p')
 
     if (divRes.childElementCount > 0) {
         divRes.removeChild(divRes.firstChild)
@@ -12,9 +25,15 @@ function count() {
 
     divRes.appendChild(outputPar)
 
-    for (var c = start; c <= end; c+=step) {
-        outputPar.innerHTML += `${c} &#x1F449 `
+    if (start < end) {
+        for (let c = start; c <= end; c+=step) {
+            outputPar.innerHTML += `${c} \u{1F449} `
+        }
+    } else {
+        for (let c = start; c >= end; c-=step) {
+            outputPar.innerHTML += `${c} \u{1F449} `
+        }
     }
-
+        
     outputPar.innerHTML += '&#x1F3C1'
 }
